1abf187fe2f3d7aec6df0d8919b71b3182f249c1
brookefzy <yuanzf@mit.edu>
2021-07-30 14:25:01 +0800

    test

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-console.log(process.env.NODE_ENV)
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/urban-change-landing-page/'
-    : '/',
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
